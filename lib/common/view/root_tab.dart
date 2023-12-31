a03992ce403495ddb4eed3e7cc115296b33cf386
tmbb-japan <woo787@naver.com>
2023-12-31 17:50:53 +0900

    test : graphQL connected

---
+++ lib/common/view/root_tab.dart
@@ -0,0 +1,15 @@
+import 'package:cost_simulator_client/common/const/colors.dart';
+import 'package:cost_simulator_client/common/layout/default_layout.dart';
+import 'package:flutter/material.dart';
+
+class RootTab extends StatelessWidget {
+  const RootTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      backgroundColor: PRIMARY_COLOR,
+      child: 
+    );
+  }
+}
--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -2,8 +2,21 @@ import 'package:cost_simulator_client/common/const/colors.dart';
 import 'package:cost_simulator_client/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    checkToken();
+  }
+
+  void checkToken() async {}
 
   @override
   Widget build(BuildContext context) {
